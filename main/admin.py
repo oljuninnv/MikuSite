c4ae4800615d2fe95ee9bd1a6163d5113dd012b9
oljuninnv <93469509+oljuninnv@users.noreply.github.com>
2022-05-15 00:47:42 +0700

    хахахахах

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import BotFunctions
 # Register your models here.
+@admin.register(BotFunctions)
+class BFAdmin(admin.ModelAdmin):
+    search_fields = ['name__startswitch','description__startswitch']
+
+    
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
+from datetime import date
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class BotFunctions (models.Model):
+    name = models.TextField(db_column = 'name',null = False,verbose_name='Название функции')
+    description = models.TextField(db_column = 'description',null = False,verbose_name='Описание функции')
+
+    class Meta:
+        verbose_name = "Описание функций"
+    def __str__(self) -> str:
+        return f"{self.name } - {self.description}"
