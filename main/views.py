bf8524af4d767b0ff2ba8ff71150728021ab0f8c
oljuninnv <93469509+oljuninnv@users.noreply.github.com>
2022-05-15 22:59:14 +0700

    papa

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BotFunctions
 # Create your views here.
 
 def index(request):
-    return render(request,'main/index.html')
+    functions = BotFunctions.objects.values()
+    return render(request,'main/index.html',{'functions':functions})
